8c4b2f0ec0d91969db075bfee3ee61e1cbbf7f1b
hasan <hasanbatis440@gmail.com>
2021-06-19 18:19:21 +0300

    ready mvp

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,38 @@
 class Project < ApplicationRecord
   has_many :tasks
   belongs_to :user
+
+  def badge_color
+    case status
+    when 'not-started'
+        'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+    end
+  end
+
+  def status
+    return 'not-started' if tasks.none?
+    if tasks.all? { |task| task.complete?}
+      'complete'
+    elsif tasks.any? { |task| task.in_progress? ||task.complete?}
+      'in-progress'
+    else
+      'not-started'
+    end
+  end
+
+  def percent_complete
+    return 0 if tasks.none?
+
+    ((total_task_complete.to_f / total_task) * 100).round
+  end
+  def total_task_complete
+    complete_tasks = tasks.select { |task| task.complete? }.count
+  end
+  def total_task
+   tasks.count
+  end
 end
